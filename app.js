f91a3ef36e21668c4cab0b9056c6f952476b0ddd
DerrickDipondo <derrickdipondo@gmail.com>
2024-12-20 22:49:14 +0300

    Complete Outlook

---
+++ app.js
@@ -0,0 +1,7 @@
+
+        <button id="toggleTheme">Dark/Light</button>
+
+        <!--Displaying Beer Images for Upvote-->
+        <div id="beerImages">
+            <!-- The beer images will be dynamically loaded here when a search is performed -->
+        </div>
